b903e0d7b228a24267da2227ef2d060e85a0ea47
Saeed-Senior <nohchosaid@mail.ru>
2023-03-29 01:22:59 +0300

    first

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Todos from "./components/Todos";
 
 function App() {
   return (
-    <div className="App bg-gray-900 text-white">
-      <h1 className="text-center text-5xl font-bold text-red-50 py-10">Todo</h1>
+    <div className="App bg-gray-900 text-white px-3">
+      <h1 className="text-center text-5xl font-bold text-red-500 py-10">Todo</h1>
       <Todos/>
     </div>
   );
