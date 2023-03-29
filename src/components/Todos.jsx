a2257b2ff36cada210a68c460604785b8cfc04af
Saeed-Senior <nohchosaid@mail.ru>
2023-03-29 17:39:11 +0300

    7

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -30,8 +30,8 @@ function x(id) {
       ) : (
          <ul>
          {data.map((item, index) => {
-            return <li data-aos="zoom-out-up" className="bg-gradient-to-r from-slate-900 to-red-500 hover:from-blue-500 hover:to-slate-900 p-4 rounded uppercase flex items-center justify-between max-w-[400px] mx-auto mb-3" key={item.id}>
-               <span className={item.completed ? 'cursor-pointer line-through text-green-500' : 'cursor-pointer'}>{item.title}</span>
+            return <li data-aos="zoom-out-up" className="bg-gradient-to-r from-slate-900 to-green-500 hover:from-blue-500 hover:to-slate-900 p-4 rounded uppercase flex items-center justify-between max-w-[400px] mx-auto mb-3" key={item.id}>
+               <span className={item.completed ? 'cursor-pointer line-through text-red-500' : 'cursor-pointer'}>{item.title}</span>
                <button onClick={() => x(item.id)} className="max-w-[40px]"><img className="w-full" src={del} alt="" /></button>
             </li>
          })}
